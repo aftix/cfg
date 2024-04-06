ae12ad63cbee76a786ca345e8ed9e642f31dae8c
aftix <aftix@aftix.xyz>
2024-04-06 18:51:07 -0500

    Moved hypr and wayland configuration to home manager

---
+++ home/aftix/waybar.nix
@@ -0,0 +1,12 @@
+{ config, upkgs, ... }:
+
+{
+  home.packages = with upkgs; [
+    waybar
+  ];
+
+  programs.waybar.enable = true;
+
+  # Fairly complicated and unchanging, just sync
+  xdg.configFile."waybar".source = ./_external.waybar;
+}
