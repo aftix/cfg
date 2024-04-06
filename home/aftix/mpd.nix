0a435feb54fb4dd400e185c8a843aa911d0c0c43
aftix <aftix@aftix.xyz>
2024-04-06 18:07:03 -0500

    Moved ncmpcpp configuration to home manager

--- home/aftix/mpd.nix
+++ home/aftix/mpd.nix
@@ -32,5 +32,12 @@
     ];
   };
 
+  xdg.configFile."ncmpcpp/config".text = ''
+    ncmpcpp_directory = ${config.home.homeDirectory}/.config/ncmpcpp
+    lyrics_directory = ${config.home.homeDirectory}/.local/share/lyrics
+    mpd_host = 127.0.0.1
+    mpd_port = 6600
+    mpd_music_dir = ${config.home.homeDirectory}/music
+  '';
 }
 
