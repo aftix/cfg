f40df0b7f25b80b15490c02c3c5e4f5a7c26a0a3
aftix <aftix@aftix.xyz>
2024-04-06 17:01:02 -0500

    moved elvish configuration to be managed by home manager

---
+++ home/aftix/elvish.nix
@@ -0,0 +1,15 @@
+{ config, ... }:
+
+{
+  # Too complicated for home manager, just sync with _external configuration
+  xdg.configFile.".config/elvish".source = ./_external.elvish;
+
+  # This changes often, just symlink it to the repo
+  # Currently config.lib.file.mkOutOfStoreSymlink doesn't work on unstable, activation script workaround
+  home.activation = {
+    updateBookmarks = ''
+      export ROOT="${config.home.homeDirectory}/src/cfg/home/aftix"
+      ln -sf "$ROOT/_external/bookmarks" .config/bookmarks
+    '';
+  };
+}
