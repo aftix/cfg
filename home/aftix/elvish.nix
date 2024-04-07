1863f294fe7b59487028c4958508d085a1980511
aftix <aftix@aftix.xyz>
2024-04-06 20:06:57 -0500

    Final configuration for ~/.config to be a tmpfs

--- home/aftix/elvish.nix
+++ home/aftix/elvish.nix
@@ -3,13 +3,4 @@
 {
   # Too complicated for home manager, just sync with _external configuration
   xdg.configFile."elvish".source = ./_external.elvish;
-
-  # This changes often, just symlink it to the repo
-  # Currently config.lib.file.mkOutOfStoreSymlink doesn't work on unstable, activation script workaround
-  home.activation = {
-    updateBookmarks = ''
-      export ROOT="${config.home.homeDirectory}/src/cfg/home/aftix"
-      ln -sf "$ROOT/_external/bookmarks" .config/bookmarks
-    '';
-  };
 }
