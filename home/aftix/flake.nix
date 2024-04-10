913cac89aa3ebacf577455d9f30f903d2e9de815
aftix <aftix@aftix.xyz>
2024-04-09 22:18:10 -0500

    Moved firefox configuration to home-manager

--- home/aftix/flake.nix
+++ home/aftix/flake.nix
@@ -4,6 +4,8 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     stablepkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nur.url = "github:nix-community/NUR";
+
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     impermanence.url = "github:nix-community/impermanence";
@@ -12,6 +14,7 @@
   outputs = {
     nixpkgs,
     stablepkgs,
+    nur,
     home-manager,
     impermanence,
     ...
@@ -20,11 +23,9 @@
     lib = nixpkgs.lib // home-manager.lib;
     upkgs = import nixpkgs {
       inherit system;
+      overlays = [nur.overlay];
       config.allowUnfreePredicate = pkg:
-        builtins.elem (nixpkgs.lib.getName pkg) [
-          "discord"
-          "vault"
-        ];
+        builtins.elem (nixpkgs.lib.getName pkg) ["discord" "vault" "pay-by-privacy"];
     };
     spkgs = import stablepkgs {inherit system;};
     extraSpecialArgs = {
