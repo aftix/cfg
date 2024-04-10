4c4f966f8299029e8cf3b5cda1c3819a17d1ff1a
aftix <aftix@aftix.xyz>
2024-04-10 09:33:09 -0500

    Added environment variables to move trash program defaults from ~ to XDG standard

--- home/aftix/aftix.nix
+++ home/aftix/aftix.nix
@@ -379,6 +379,13 @@
         selection-color = #eed49f
         background-color = #24273a
       '';
+
+      "npm/npmrc".text = ''
+        prefix=''${XDG_DATA_HOME}/npm
+        cache=''${XDG_CACHE_HOME}/npm
+        init-module=''${XDG_CONFIG_HOME}/npm/config/npm-init.js
+        logs-dir=''${XDG_CACHE_HOME}/npm/logs
+      '';
     };
   };
 }
