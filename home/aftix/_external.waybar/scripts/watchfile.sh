5f81e3f97ecd02471ef13c11be62a16e06bac234
aftix <aftix@aftix.xyz>
2024-04-12 09:21:44 -0500

    Moved waybar configuration to inside nix

--- home/aftix/_external.waybar/scripts/watchfile.sh
+++
@@ -1,7 +0,0 @@
-#!/run/current-system/sw/bin/bash
-
-[ -f /var/run/backupdisk.pid ] && echo '{"text": "Backing up disk"}'
-inotifywait -m /var/run --include "backupdisk\\.pid" -e create -e delete 2>/dev/null | while read -r line ; do
-  grep -Fq '/var/run DELETE backupdisk.pid' <<< "$line" && echo '{}'
-  grep -Fq '/var/run CREATE backupdisk.pid' <<< "$line" && echo '{"text": "Backing up disk"}'
-done
