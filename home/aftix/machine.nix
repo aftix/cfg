57b13176059e9753fd7d59402d772a61a91ff890
aftix <aftix@aftix.xyz>
2024-04-18 11:18:45 -0500

    Created aftix user machine-specific nixos import, updated hyprland monitors

---
+++ home/aftix/machine.nix
@@ -0,0 +1,7 @@
+{...}: {
+  # Configuration for my specific displays
+  wayland.windowManager.hyprland.settings.monitor = [
+    "desc:ASUSTek COMPUTER INC ASUS VG27W 0x0001995C,preferred,0x0,1"
+    ",preferred,auto,1"
+  ];
+}
