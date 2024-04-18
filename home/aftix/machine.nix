a926c4d4a98e403044b8e75c85a1539d3891b794
aftix <aftix@aftix.xyz>
2024-04-18 18:11:13 -0500

    - Added second monitor to hyprland in machine.nix - Setup default monitor workspaces - Setup different waybars for different monitors - Setup autolaunch programs in hyprland - Added kitty session for irc - Opening a scratchpad will now launch kitty if it's empty

--- home/aftix/machine.nix
+++ home/aftix/machine.nix
@@ -1,7 +1,31 @@
-{...}: {
+{mylib, ...}: {
   # Configuration for my specific displays
-  wayland.windowManager.hyprland.settings.monitor = [
-    "desc:ASUSTek COMPUTER INC ASUS VG27W 0x0001995C,preferred,0x0,1"
-    ",preferred,auto,1"
-  ];
+  wayland.windowManager.hyprland.settings = {
+    monitor = [
+      "desc:ASUSTek COMPUTER INC ASUS VG27W 0x0001995C,preferred,0x0,1"
+      # transform 1 is 90 degree rotation cw
+      "desc:ViewSonic Corporation VX2703 SERIES T8G132800478,preferred,2560x-180,1,transform,1"
+      ",preferred,auto,1"
+    ];
+
+    workspace = [
+      "1, persistent:true, monitor:desc:ASUSTek COMPUTER INC ASUS VG27W 0x0001995C, default:true"
+      "2, persistent:true, monitor:desc:ViewSonic Corporation VX2703 SERIES T8G132800478, default:true"
+      "2, layoutopt:orientation:top"
+    ];
+  };
+
+  xdg.configFile."hypr/hyprpaper.conf".text = mylib.toHyprCfg {
+    preload = [
+      "/home/aftix/.local/share/wallpaper/X0pSg4c.jpg"
+      "/home/aftix/.local/share/wallpaper/dzLwX8H.jpg"
+    ];
+
+    wallpaper = [
+      "desc:ASUSTek COMPUTER INC ASUS VG27W 0x0001995C,/home/aftix/.local/share/wallpaper/X0pSg4c.jpg"
+      "desc:ViewSonic Corporation VX2703 SERIES T8G132800478,/home/aftix/.local/share/wallpaper/dzLwX8H.jpg"
+    ];
+
+    splash = true;
+  };
 }
