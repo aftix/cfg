87b58a800e4cf16a5bea5a2611a8024b29615c44
aftix <aftix@aftix.xyz>
2024-05-04 13:04:21 -0500

    Improved hyprland configuration options

--- hardware/hamilton-home.nix
+++ hardware/hamilton-home.nix
@@ -1,17 +1,44 @@
-_: {
-  # Configuration for my specific displays
-  wayland.windowManager.hyprland.settings = {
-    monitor = [
-      "desc:ASUSTek COMPUTER INC ASUS VG27W 0x0001995C,preferred,0x0,1"
-      # transform 1 is 90 degree rotation cw
-      "desc:ViewSonic Corporation VX2703 SERIES T8G132800478,preferred,2560x-180,1,transform,1"
-      ",preferred,auto,1"
+{config, ...}: {
+  my.hyprland = let
+    asus = "ASUSTek COMPUTER INC ASUS VG27W 0x0001995C";
+    viewSonic = "ViewSonic Corporation VX2703 SERIES T8G132800478";
+    inherit (config.my.hyprland) transforms;
+  in {
+    extraMonitor = [
+      {
+        desc = asus;
+        position = "0x0";
+      }
+      {
+        desc = viewSonic;
+        position = "2560x-180";
+        transform = transforms."90";
+      }
     ];
 
-    workspace = [
-      "1, persistent:true, monitor:desc:ASUSTek COMPUTER INC ASUS VG27W 0x0001995C, default:true"
-      "2, persistent:true, monitor:desc:ViewSonic Corporation VX2703 SERIES T8G132800478, default:true"
-      "2, layoutopt:orientation:top"
+    extraWorkspace = [
+      {
+        name = "1";
+        options = [
+          "persistent:true"
+          "monitor:desc:${asus}"
+          "default:true"
+        ];
+      }
+      {
+        name = "2";
+        options = [
+          "persistent:true"
+          "monitor:desc:${viewSonic}"
+          "default:true"
+        ];
+      }
+      {
+        name = "2";
+        options = [
+          "layoutopt:orientation:top"
+        ];
+      }
     ];
   };
 }
