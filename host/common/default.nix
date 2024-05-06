fe4eea979eb3b78de2e2bff57250935aa75af759
aftix <aftix@aftix.xyz>
2024-05-05 23:20:04 -0500

    Added cachix caches

--- host/common/default.nix
+++ host/common/default.nix
@@ -23,6 +23,7 @@
   config = {
     environment.systemPackages = with pkgs; [
       home-manager
+      cachix
 
       killall
       curl
@@ -44,6 +45,15 @@
       settings = {
         experimental-features = ["nix-command" "flakes"];
         use-xdg-base-directories = true;
+
+        substituters = [
+          "https://nix-community.cachix.org"
+          "https://cache.nixos.org"
+        ];
+        trusted-public-keys = [
+          "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+          "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        ];
       };
 
       optimise.automatic = true;
