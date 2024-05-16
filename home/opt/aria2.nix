1cece6e1c2bc7272e3da01723c1e704e8db094d2
aftix <aftix@aftix.xyz>
2024-05-15 20:37:43 -0500

    - Updated flake inputs - changed overrides to refer to final instead of prev - changed writeShellScript to writeShellApplication

--- home/opt/aria2.nix
+++ home/opt/aria2.nix
@@ -4,17 +4,21 @@
   ...
 }: {
   nixpkgs.overlays = [
-    (_: prev: {
-      aria2d = prev.writeScriptBin "aria2d" (let
-        rpcDir = "${config.xdg.configHome}/aria2";
-        rpcFile = "${rpcDir}/aria2d.env";
-      in ''
-        #!${prev.stdenv.shell}
-        mkdir -p "${rpcDir}"
-        dd if=/dev/urandom of=/dev/stdout bs=64 count=1 2>/dev/null | base64 | tr -d '\n=*' | xargs printf "ARIA2_RPC_TOKEN=%s" > "${rpcFile}"
-        source "${rpcFile}"
-        ${prev.aria2}/bin/aria2c --conf-path="${rpcDir}/aria2d.conf" --rpc-secret="$ARIA2_RPC_TOKEN"
-      '');
+    (final: _: {
+      aria2d = final.writeShellApplication {
+        name = "aria2d";
+        runtimeInputs = with final; [aria2];
+        text = let
+          rpcDir = "${config.xdg.configHome}/aria2";
+          rpcFile = "${rpcDir}/aria2d.env";
+        in ''
+          mkdir -p "${rpcDir}"
+          dd if=/dev/urandom of=/dev/stdout bs=64 count=1 2>/dev/null | base64 | tr -d '\n=*' | xargs printf "ARIA2_RPC_TOKEN=%s" > "${rpcFile}"
+          # shellcheck source=/dev/null
+          source "${rpcFile}"
+          aria2c --conf-path="${rpcDir}/aria2d.conf" --rpc-secret="$ARIA2_RPC_TOKEN"
+        '';
+      };
     })
   ];
   home.packages = with pkgs; [aria2 aria2d python312Packages.aria2p];
