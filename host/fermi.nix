6980649b040690cbf4c3a92ec5a0e64288ad82d8
aftix <aftix@aftix.xyz>
2024-05-17 13:34:37 -0500

    Fixed fermi configuration for deployment

--- host/fermi.nix
+++ host/fermi.nix
@@ -27,6 +27,9 @@ in {
       aftgraphs = true;
       blog = true;
       searx.enable = true;
+
+      ip = "170.130.165.174";
+      ipv6 = "2a0b:7140:8:1:5054:ff:fe84:ed8c";
     };
 
     znc.enable = true;
