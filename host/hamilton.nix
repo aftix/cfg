7c710c6c6e782cdcca33de36198bbcc3bbb69477
aftix <aftix@aftix.xyz>
2024-06-10 11:46:36 -0500

    Changed from sddm to regreet/greetd

--- host/hamilton.nix
+++ host/hamilton.nix
@@ -54,6 +54,11 @@ in {
 
     backup.bucket = "aftix-hamilton-backup";
     uefi = true;
+
+    greeterCfgExtra = ''
+      monitor=desc:ASUSTek COMPUTER INC ASUS VG27W 0x0001995C,preferred,0x0,1
+      monitor=desc:ViewSonic Corporation VX2703 SERIES T8G132800478,preferred,2560x-180,1,transform,1
+    '';
   };
 
   environment = {
--- host/opt/display.nix
+++ host/opt/display.nix
@@ -2,67 +2,137 @@
   pkgs,
   lib,
   hyprPkgs,
+  config,
   ...
 }: let
-  inherit (lib) mkDefault;
+  inherit (lib) mkDefault mkOverride;
+  inherit (lib.options) mkOption;
 in {
-  environment.systemPackages = with pkgs; [
-    kdePackages.kwin
-  ];
-
-  nix.settings = {
-    substituters = ["https://hyprland.cachix.org"];
-    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+  options.my.greeterCfgExtra = mkOption {
+    default = "";
+    type = lib.types.str;
   };
 
-  programs.hyprland = {
-    enable = mkDefault true;
-    package = hyprPkgs.hyprland;
-  };
+  config = let
+    wallpaper = "${hyprPkgs.hyprland}/share/hyprland/wall2.png";
+  in {
+    nix.settings = {
+      substituters = ["https://hyprland.cachix.org"];
+      trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+    };
 
-  console.useXkbConfig = true;
+    environment.systemPackages = with pkgs; [
+      rose-pine-cursor
+      hyprpaper
+    ];
 
-  services = {
-    displayManager.sddm = {
-      enable = true;
-      wayland.enable = true;
-      autoNumlock = true;
-      settings = {
-        General = {
-          GreeterEnvironment = "QT_WAYLAND_SHELL_INTEGRATION=layer-shell";
+    programs = {
+      hyprland = {
+        enable = mkDefault true;
+        package = hyprPkgs.hyprland;
+      };
+
+      regreet = {
+        enable = true;
+        settings = {
+          commands = {
+            reboot = ["loginctl" "reboot"];
+            poweroff = ["loginctl" "poweroff"];
+          };
+
+          GTK = {
+            application_prefer_dark_theme = true;
+            cursor_theme_name = "BreezeX-RosePine-Linux";
+          };
+
+          background = {
+            path = wallpaper;
+            fit = "Contain";
+          };
         };
-        Autologin = {
-          Session = mkDefault "hyprland";
-          User = mkDefault "aftix";
-        };
-        Wayland = {
-          CompositorCommand = "kwin";
-        };
-        Theme.EnableAvatars = true;
       };
     };
 
-    xserver.xkb = {
-      layout = mkDefault "us";
-      variant = mkDefault "dvorak";
-      options = mkDefault "compose:prsc,caps:escape";
-    };
-  };
+    console.useXkbConfig = true;
 
-  fonts.packages = with pkgs; [
-    inconsolata
-    dejavu_fonts
-    noto-fonts
-    noto-fonts-cjk
-    noto-fonts-emoji
-    liberation_ttf
-    fira-code
-    fira-code-symbols
-    dina-font
-    proggyfonts
-    nerdfonts
-    font-awesome
-    office-code-pro
-    cantarell-fonts
-  ];
+    services = {
+      greetd = {
+        enable = true;
+        settings = {
+          terminal.vt = 1;
+
+          initial_session = {
+            command = "${hyprPkgs.hyprland}/bin/Hyprland";
+            user = mkOverride 990 "aftix";
+          };
+
+          default_session = let
+            paperCfg = pkgs.writeTextFile {
+              name = "hyprpaper-cfg";
+              text = ''
+                preload = ${wallpaper}
+                splash = false
+                ipc = off
+                wallpaper = ,${wallpaper}
+              '';
+            };
+
+            greetCfg = pkgs.writeTextFile {
+              name = "greetd-cfg";
+              text = ''
+                exec-once = ${config.programs.regreet.package}/bin/regreet; ${hyprPkgs.hyprland}/bin/hyprctl dispatch exit
+
+                monitor=,preferred,auto,1
+                input {
+                  touchpad {
+                    natural_scroll=false
+                  }
+                  kb_layout=us
+                  kb_options=compose:prsc
+                  kb_options=caps:escape
+                  kb_variant=dvorak
+                  numlock_by_default=true
+                  repeat_delay=300
+                  repeat_rate=60
+                  sensitivity=0
+                }
+                misc {
+                  force_default_wallpaper=0
+                }
+                exec-once = ${pkgs.hyprpaper}/bin/hyprpaper --config ${paperCfg}
+
+                ${config.my.greeterCfgExtra}
+              '';
+            };
+          in {
+            command = "${hyprPkgs.hyprland}/bin/Hyprland --config ${greetCfg}";
+            user = mkOverride 990 "aftix";
+          };
+        };
+      };
+
+      xserver.xkb = {
+        layout = mkDefault "us";
+        variant = mkDefault "dvorak";
+        options = mkDefault "compose:prsc,caps:escape";
+      };
+    };
+
+    fonts.packages = with pkgs; [
+      inconsolata
+      dejavu_fonts
+      noto-fonts
+      noto-fonts-cjk
+      noto-fonts-emoji
+      liberation_ttf
+      fira-code
+      fira-code-symbols
+      dina-font
+      proggyfonts
+      nerdfonts
+      font-awesome
+      office-code-pro
+      cantarell-fonts
+    ];
+  };
 }
