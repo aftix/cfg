2ffed00c2d2b985fbf9c460cc1a59ae188067fe1
aftix <aftix@aftix.xyz>
2024-09-29 15:08:27 -0500

    packages: Added package for nu_plugin_desktop_notifications.

--- overlay.nix
+++ overlay.nix
@@ -21,4 +21,5 @@ inputs: final: prev: {
   nu_plugin_audio_hook = final.callPackage ./packages/nu_plugin_audio_hook.nix {};
   nu_plugin_compress = final.callPackage ./packages/nu_plugin_compress.nix {};
   nu_plugin_dbus = final.callPackage ./packages/nu_plugin_dbus.nix {};
+  nu_plugin_desktop_notifications = final.callPackage ./packages/nu_plugin_desktop_notifications.nix {};
 }
