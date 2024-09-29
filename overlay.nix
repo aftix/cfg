f8dea5f865e7f20cf35380807b381890c77a6369
aftix <aftix@aftix.xyz>
2024-09-29 15:54:21 -0500

    packages: Added package for nu_plugin_strutils.

--- overlay.nix
+++ overlay.nix
@@ -25,4 +25,5 @@ inputs: final: prev: {
   nu_plugin_dns = final.callPackage ./packages/nu_plugin_dns.nix {};
   nu_plugin_endecode = final.callPackage ./packages/nu_plugin_endecode.nix {};
   nu_plugin_explore = final.callPackage ./packages/nu_plugin_explore.nix {};
+  nu_plugin_strutils = final.callPackage ./packages/nu_plugin_strutils.nix {};
 }
