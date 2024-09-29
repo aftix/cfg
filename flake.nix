e634020fd84322bfa4d1f9281b2c35cbc7a40ba0
aftix <aftix@aftix.xyz>
2024-09-29 13:38:28 -0500

    flake: Moved overlay into separate file

--- flake.nix
+++ flake.nix
@@ -133,27 +133,7 @@
   } @ inputs: let
     system = "x86_64-linux";
 
-    overlay = final: prev: {
-      inherit (stablepkgs.legacyPackages.${final.system}) znc freshrss fail2ban transmission_4 hyprpaper xdg-desktop-portal-hyprland;
-
-      carapace =
-        (prev.carapace.overrideAttrs {
-          src = inputs.carapace;
-          vendorHash = "sha256-biJN+WjNK/Fjjd3+ihZcFCu75fup3g9R6lIa6qHco5I=";
-        })
-        .override {buildGoModule = final.buildGo123Module;};
-
-      heisenbridge = prev.heisenbridge.overridePythonAttrs (oldAttrs: rec {
-        version = "1.15.0";
-        src = final.fetchFromGitHub {
-          owner = "hifi";
-          repo = oldAttrs.pname;
-          rev = "refs/tags/v${version}";
-          sha256 = "sha256-4K6Sffu/yKHkcoNENbgpci2dbJVAH3vVkogcw/IYpnw=";
-        };
-      });
-    };
-
+    overlay = import ./overlay.nix inputs;
     getModules = atPath: let
       getFilename = path: nixpkgs.lib.lists.last (nixpkgs.lib.strings.split "/" (builtins.toString path));
       mapAppendPath = path: f: p: v: f (nixpkgs.lib.path.append path p) v;
---
+++ overlay.nix
@@ -0,0 +1,20 @@
+inputs: final: prev: {
+  inherit (inputs.stablepkgs.legacyPackages.${final.system}) znc freshrss fail2ban transmission_4 hyprpaper xdg-desktop-portal-hyprland;
+
+  carapace =
+    (prev.carapace.overrideAttrs {
+      src = inputs.carapace;
+      vendorHash = "sha256-biJN+WjNK/Fjjd3+ihZcFCu75fup3g9R6lIa6qHco5I=";
+    })
+    .override {buildGoModule = final.buildGo123Module;};
+
+  heisenbridge = prev.heisenbridge.overridePythonAttrs (oldAttrs: rec {
+    version = "1.15.0";
+    src = final.fetchFromGitHub {
+      owner = "hifi";
+      repo = oldAttrs.pname;
+      rev = "refs/tags/v${version}";
+      sha256 = "sha256-4K6Sffu/yKHkcoNENbgpci2dbJVAH3vVkogcw/IYpnw=";
+    };
+  });
+}
