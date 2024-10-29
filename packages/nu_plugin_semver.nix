ff71da60ed8a262b2e86705ee0940ba7e602566e
aftix <aftix@aftix.xyz>
2024-10-29 17:13:51 -0500

    flake: updated inputs

--- packages/nu_plugin_semver.nix
+++ packages/nu_plugin_semver.nix
@@ -7,15 +7,15 @@
 }:
 rustPlatform.buildRustPackage rec {
   pname = "nu_plugin_semver";
-  version = "2bc0b459f9062cde9c7a7f516b8057ea9eece670";
+  version = "0.9.0";
 
   src = fetchFromGitHub {
     owner = "abusch";
     repo = pname;
-    rev = version;
+    rev = "v${version}";
     sha256 = "sha256-SXqDsywED76O05pyo5Jw1nxkw6BWiy5NyRMZ8tgHFu0=";
   };
-  cargoHash = "sha256-d6IG+7FUg10zY6GiUR/RKeWNFLCX6Xp44j55quWwzo4=";
+  cargoHash = "sha256-r6lrgKWKY1NBDOFJYoX45rE+9E8Cxr2Di/x/4FFQTMU=";
 
   nativeBuildInputs = lib.optionals stdenv.hostPlatform.isDarwin [rustPlatform.bindgenHook];
   buildInputs = lib.optionals stdenv.hostPlatform.isDarwin [
@@ -26,7 +26,7 @@ rustPlatform.buildRustPackage rec {
   meta = with lib; {
     description = "This is a plugin for the nu shell to manipulate strings representing versions that conform to the SemVer specification.";
     mainProgram = "nu_plugin_semver";
-    homepage = "https://github.com/abusch/nu_plugin_semver/tree/${version}";
+    homepage = "https://github.com/abusch/nu_plugin_semver";
     license = licenses.mit;
     platforms = with platforms; all;
   };
