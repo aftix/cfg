2f91ef968268c86089aa56572f6c7d56c4d84e2d
aftix <aftix@aftix.xyz>
2024-11-08 16:28:35 -0600

    flake: removed cache.thalheim.io substituter

--- flake.nix
+++ flake.nix
@@ -100,14 +100,12 @@
       "https://nix-community.cachix.org"
       "https://helix.cachix.org"
       "https://cache.nixos.org"
-      "https://cache.thalheim.io"
       "https://attic.aftix.xyz/cfg-actions"
     ];
     trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs="
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      "cache.thalheim.io-1:R7msbosLEZKrxk/lKxf9BTjOOH7Ax3H0Qj0/6wiHOgc="
       "cfg-actions:R9aJEQdcJT8NrUh1yox2FgZfmzRrKi6MAobbfuRvv3g="
     ];
 
