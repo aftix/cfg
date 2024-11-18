e3eecaaedb4c85c2bdb104e31b0417c5aa15ccd6
aftix <aftix@aftix.xyz>
2024-11-18 11:50:05 -0600

    packages: updated packages with unstable versions to specify git shas

--- packages/nu_plugin_endecode.nix
+++ packages/nu_plugin_endecode.nix
@@ -13,7 +13,7 @@ rustPlatform.buildRustPackage rec {
     domain = "codeberg.org";
     owner = "kaathewise";
     repo = "nugins";
-    rev = version;
+    rev = "4d988d5e7772843a3b592a557e71a52d2a956ed0";
     sha256 = "sha256-gxB5OB2hehAX6QCxxLgWmWx0H0AaLGET1/lCRfo5uSo=";
   };
   cargoHash = "sha256-fZVG6JcDbVTaYiS60oLb/QJX6gljrynK6M/zFmci1GE=";
@@ -32,7 +32,7 @@ rustPlatform.buildRustPackage rec {
   meta = with lib; {
     description = "A plugin with various encoding schemes, from Crockford's base-32 to HTML entity escaping.";
     mainProgram = "nu_plugin_endecode";
-    homepage = "https://codeberg.org/kaathewise/nu-plugin/src/commit/${version}";
+    homepage = "https://codeberg.org/kaathewise/nugins";
     license = licenses.mpl20;
     platforms = with platforms; all;
   };
