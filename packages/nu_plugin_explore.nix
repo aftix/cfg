776a9c740d43372fbaae3f19d14c464daf264910
aftix <aftix@aftix.xyz>
2024-11-18 18:52:56 -0600

    flake: updated inputs

--- packages/nu_plugin_explore.nix
+++ packages/nu_plugin_explore.nix
@@ -15,7 +15,7 @@ rustPlatform.buildRustPackage rec {
     rev = version;
     sha256 = "sha256-EX44IzxEJWK/kzLJ+edUUbGjZMbXJeM+p2/E4kGzrfM=";
   };
-  cargoHash = "sha256-sBZhP6m9GxnC5DqUPPs977gQEn/cFVQGfjBQ3ZudeVI=";
+  cargoHash = "sha256-yQIJsZjUeJkeq+psIyy88IB/cjsEtiNRCnZN/UKVV6Y=";
 
   nativeBuildInputs = lib.optionals stdenv.hostPlatform.isDarwin [rustPlatform.bindgenHook];
   buildInputs = lib.optionals stdenv.hostPlatform.isDarwin [
