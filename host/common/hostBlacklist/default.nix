5e87920078687f7272f1a8b2058833d7dbf30ffa
aftix <aftix@aftix.xyz>
2024-12-18 14:26:50 -0600

    host: common: hostBlacklist: allow storage.googleapis.com

--- host/common/hostBlacklist/default.nix
+++ host/common/hostBlacklist/default.nix
@@ -1,10 +1,21 @@
-{config, ...}: let
+{
+  config,
+  pkgs,
+  ...
+}: let
   inherit (config.dep-inject) inputs;
+
+  patchedHosts2 =
+    pkgs.runCommandLocal "hosts2-patched" {
+    }
+    ''
+      sed '/^0.0.0.0 storage\.googleapis\.com$/ d' "${inputs.hostsBlacklist}/hosts/hosts2" > "$out"
+    '';
 in {
   networking.hostFiles = [
     "${inputs.hostsBlacklist}/hosts/hosts0"
     "${inputs.hostsBlacklist}/hosts/hosts1"
-    "${inputs.hostsBlacklist}/hosts/hosts2"
+    "${patchedHosts2}"
     "${inputs.hostsBlacklist}/hosts/hosts3"
     ./personal-blacklist
   ];
