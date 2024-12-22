857f6045f313b2a7eacaf932563e85cc91e40d78
aftix <aftix@aftix.xyz>
2024-12-22 04:30:08 -0600

    host: common: Separated coffeepaste into DynamicUser with StateDirectory

--- host/common/coffeepaste.nix
+++ host/common/coffeepaste.nix
@@ -4,7 +4,6 @@
   pkgs,
   ...
 }: let
-  inherit (lib) mkDefault;
   inherit (lib.options) mkOption mkEnableOption;
 
   cfg = config.services.coffeepaste;
@@ -12,7 +11,7 @@
   configFile = (pkgs.formats.toml {}).generate "config.toml" {
     inherit (cfg) url max_file_size expiration_days;
     listen = "${cfg.listenAddr}:${builtins.toString cfg.listenPort}";
-    data = "${cfg.dataDir}";
+    data = "/var/lib/private/coffeepaste";
   };
   configDrv = pkgs.runCommandLocal "coffeepaste-config" {} ''
     mkdir -p $out
@@ -46,11 +45,6 @@ in {
       type = lib.types.ints.unsigned;
     };
 
-    dataDir = mkOption {
-      default = "/var/lib/coffeepaste";
-      type = lib.types.path;
-    };
-
     max_file_size = mkOption {
       default = 15000000;
       type = lib.types.ints.unsigned;
@@ -63,54 +57,39 @@ in {
   };
 
   config = lib.mkIf cfg.enable {
-    users = {
-      users.${cfg.user} = {
-        isSystemUser = mkDefault true;
-        group = mkDefault cfg.group;
-        shell = mkDefault (lib.getExe' pkgs.shadow "nologin");
+    systemd.services.coffeepaste = {
+      wants = ["network.target"];
+      after = ["network.target"];
+      wantedBy = ["multi-user.target"];
+      unitConfig.Description = "A neat pastebin";
+
+      serviceConfig = {
+        User = cfg.user;
+        Group = cfg.group;
+        WorkingDirectory = "${configDrv}";
+        DynamicUser = true;
+        StateDirectory = "coffeepaste";
+
+        CapabilityBoundingSet = config.my.systemdCapabilities;
+        LockPersonality = true;
+        MemoryDenyWriteExecute = true;
+        NoNewPrivileges = true;
+        PrivateDevices = true;
+        PrivateUsers = true;
+        ProtectHome = true;
+        ProtectHostname = true;
+        ProtectKernelModules = true;
+        ProtectKernelTunables = true;
+        ProtectProc = "invisible";
+        ProtectSystem = "strict";
+        RemoveIPC = true;
+        RestrictNamespaces = true;
+        RestrictRealtime = true;
+        RestrictSUIDSGID = true;
+        SystemCallArchitectures = "native";
+        UMask = "0027";
       };
-      groups.${cfg.group} = {};
-    };
-
-    systemd = {
-      tmpfiles.rules = [
-        "d ${cfg.dataDir} 0750 ${cfg.user} ${cfg.group} -"
-      ];
-
-      services.coffeepaste = {
-        wants = ["network.target"];
-        after = ["network.target"];
-        wantedBy = ["multi-user.target"];
-        unitConfig.Description = "A neat pastebin";
-
-        serviceConfig = {
-          User = cfg.user;
-          Group = cfg.group;
-          WorkingDirectory = "${configDrv}";
-          ReadWritePaths = cfg.dataDir;
-
-          CapabilityBoundingSet = config.my.systemdCapabilities;
-          LockPersonality = true;
-          MemoryDenyWriteExecute = true;
-          NoNewPrivileges = true;
-          PrivateDevices = true;
-          PrivateTmp = true;
-          PrivateUsers = true;
-          ProtectHome = true;
-          ProtectHostname = true;
-          ProtectKernelModules = true;
-          ProtectKernelTunables = true;
-          ProtectProc = "invisible";
-          ProtectSystem = "strict";
-          RemoveIPC = true;
-          RestrictNamespaces = true;
-          RestrictRealtime = true;
-          RestrictSUIDSGID = true;
-          SystemCallArchitectures = "native";
-          UMask = "0027";
-        };
-        script = "${lib.getExe pkgs.coffeepaste}";
-      };
+      script = "${lib.getExe pkgs.coffeepaste}";
     };
   };
 }
