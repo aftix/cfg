bfd56aec04e23671c4ec3ea21d0994b2edaa15aa
aftix <aftix@aftix.xyz>
2024-12-31 14:08:48 -0600

    host: common: hostBlacklist: Add option for removing lines from blacklists

--- host/common/hostBlacklist/default.nix
+++ host/common/hostBlacklist/default.nix
@@ -1,22 +1,58 @@
 {
   config,
+  lib,
   pkgs,
   ...
 }: let
   inherit (config.dep-inject) inputs;
 
-  patchedHosts2 =
-    pkgs.runCommandLocal "hosts2-patched" {
+  patchHost = {
+    name,
+    file,
+  }: let
+    removeCmds = lib.flip builtins.map config.my.blacklist.removeLines (regex: "sed -i'' -e '/${regex}/ d' \"$out\"");
+  in
+    pkgs.runCommandLocal name {
+      src = file;
     }
     ''
-      sed '/^0.0.0.0 storage\.googleapis\.com$/ d' "${inputs.hostsBlacklist}/hosts/hosts2" > "$out"
+      cp "$src" "$out"
+      ${lib.strings.concatLines removeCmds}
     '';
 in {
-  networking.hostFiles = [
-    "${inputs.hostsBlacklist}/hosts/hosts0"
-    "${inputs.hostsBlacklist}/hosts/hosts1"
-    "${patchedHosts2}"
-    "${inputs.hostsBlacklist}/hosts/hosts3"
-    ./personal-blacklist
+  options.my.blacklist.removeLines = lib.mkOption {
+    default = [
+      "^0.0.0.0 storage\\.googleapis\\.com$"
+      "^0.0.0.0 addons\\.mozilla\\.org$"
+    ];
+
+    description = ''
+      List of regexes that remove lines from the hosts blacklists on match.
+    '';
+
+    type = with lib.types; listOf str;
+  };
+
+  config.networking.hostFiles = builtins.map patchHost [
+    {
+      file = "${inputs.hostsBlacklist}/hosts/hosts0";
+      name = "patched-hosts0";
+    }
+    {
+      file = "${inputs.hostsBlacklist}/hosts/hosts1";
+      name = "patched-hosts1";
+    }
+    {
+      file = "${inputs.hostsBlacklist}/hosts/hosts2";
+      name = "patched-hosts2";
+    }
+    {
+      file = "${inputs.hostsBlacklist}/hosts/hosts3";
+      name = "patched-hosts3";
+    }
+    {
+      file = ./personal-blacklist;
+      name = "patched-personal";
+    }
   ];
 }
