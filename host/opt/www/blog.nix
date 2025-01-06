084099a22562798c219297dbed5cebed99f6e172
aftix <aftix@aftix.xyz>
2025-01-06 13:47:28 -0600

    home: opt: www: Decoupled www modules from each other to support independent deployment

--- host/opt/www/blog.nix
+++ host/opt/www/blog.nix
@@ -4,60 +4,93 @@
   ...
 }: let
   inherit (lib) mkIf;
-  inherit (lib.options) mkEnableOption;
+  inherit (lib.options) mkEnableOption mkOption;
   inherit (lib.lists) optional;
-  cfg = config.my.www;
+  wwwCfg = config.my.www;
+  cfg = config.my.www.blog;
+
+  acmeHost =
+    if cfg.acmeDomain == null
+    then cfg.domain
+    else cfg.acmeDomain;
 in {
-  options.my.www = {
+  options.my.www.blog = {
+    enable = mkEnableOption "blog";
+
+    domain = mkOption {
+      type = lib.types.str;
+    };
+
+    acmeDomain = mkOption {
+      default = wwwCfg.acmeDomain;
+      type = with lib.types; nullOr str;
+      description = "null to use \${config.my.www.blog.domain}";
+    };
+
     adventofcode = mkEnableOption "adventofcode";
     aftgraphs = mkEnableOption "aftgraphs";
-    blog = mkEnableOption "blog";
   };
 
-  config = mkIf cfg.blog {
+  config = mkIf cfg.enable {
     systemd.tmpfiles.rules =
       [
-        "d ${cfg.root}/${cfg.hostname} 0775 ${cfg.user} ${cfg.group} -"
-        "Z ${cfg.root}/${cfg.hostname} 0775 ${cfg.user} ${cfg.group} -"
+        "d ${wwwCfg.root}/${cfg.domain} 0775 ${wwwCfg.user} ${wwwCfg.group} -"
+        "Z ${wwwCfg.root}/${cfg.domain} 0775 ${wwwCfg.user} ${wwwCfg.group} -"
       ]
-      ++ optional cfg.adventofcode "d ${cfg.root}/advent2023 0775 ${cfg.user} ${cfg.group} -"
-      ++ optional cfg.aftgraphs "d ${cfg.root}/simulations 0775 ${cfg.user} ${cfg.group} -";
+      ++ optional cfg.adventofcode "d ${wwwCfg.root}/advent2023 0775 ${wwwCfg.user} ${wwwCfg.group} -"
+      ++ optional cfg.aftgraphs "d ${wwwCfg.root}/simulations 0775 ${wwwCfg.user} ${wwwCfg.group} -";
 
-    services.nginx.virtualHosts.${cfg.hostname} = {
-      root = "${cfg.root}/${cfg.hostname}";
-      serverName = "${cfg.hostname} www.${cfg.hostname}";
-      kTLS = true;
-      forceSSL = true;
-      useACMEHost = cfg.hostname;
-      extraConfig = ''
-        error_page ${builtins.toString cfg.putRequestCode} = @putrequest;
-        include /etc/nginx/bots.d/blockbots.conf;
-        include /etc/nginx/bots.d/ddos.conf;
-      '';
-
-      locations = {
-        "/".tryFiles = "$uri $uri/ =404";
-
-        "/searx/".return = mkIf cfg.searx.enable "https://${cfg.searx.subdomain}.${cfg.hostname}/?$args";
-
-        "/advent2023/" = mkIf cfg.adventofcode {
-          alias = "${cfg.root}/advent2023/";
-          extraConfig = ''
-            fancyindex on;
-            fancyindex_exact_size off;
-            fancyindex_localtime on;
-          '';
+    security.acme.certs =
+      if (acmeHost != cfg.domain)
+      then {
+        ${acmeHost}.extraDomainNames = [
+          "${cfg.domain}"
+          "www.${cfg.domain}"
+        ];
+      }
+      else {
+        ${acmeHost} = {
+          inherit (wwwCfg) group;
+          extraDomainNames = ["www.${cfg.domain}"];
         };
+      };
 
-        "/aftgraphs/" = mkIf cfg.aftgraphs {
-          alias = "${cfg.root}/simulations/";
-          extraConfig = ''
-            fancyindex on;
-            fancyindex_exact_size off;
-            fancyindex_localtime on;
-            add_header "Cross-Origin-Opener-Policy" "same-origin";
-            add_header "Cross-Origin-Embedder-Policy" "require-corp";
-          '';
+    services.nginx = {
+      enable = true;
+
+      virtualHosts.${cfg.domain} = {
+        root = "${wwwCfg.root}/${cfg.domain}";
+        serverName = "${cfg.domain} www.${cfg.domain}";
+        kTLS = true;
+        forceSSL = true;
+        useACMEHost = cfg.domain;
+        extraConfig = ''
+          include /etc/nginx/bots.d/blockbots.conf;
+          include /etc/nginx/bots.d/ddos.conf;
+        '';
+
+        locations = {
+          "/".tryFiles = "$uri $uri/ =404";
+
+          "/advent2023/" = mkIf cfg.adventofcode {
+            alias = "${wwwCfg.root}/advent2023/";
+            extraConfig = ''
+              fancyindex on;
+              fancyindex_exact_size off;
+              fancyindex_localtime on;
+            '';
+          };
+
+          "/aftgraphs/" = mkIf cfg.aftgraphs {
+            alias = "${wwwCfg.root}/simulations/";
+            extraConfig = ''
+              fancyindex on;
+              fancyindex_exact_size off;
+              fancyindex_localtime on;
+              add_header "Cross-Origin-Opener-Policy" "same-origin";
+              add_header "Cross-Origin-Embedder-Policy" "require-corp";
+            '';
+          };
         };
       };
     };
