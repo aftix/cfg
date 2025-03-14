00bfd38c59a1b245bf6f79a2f2e8be0128c2c05e
aftix <aftix@aftix.xyz>
2025-03-14 15:19:59 -0500

    packages/nu_plugin_desktop_notifications: fixed FOD fetcher

--- packages/nu_plugin_desktop_notifications.nix
+++ packages/nu_plugin_desktop_notifications.nix
@@ -5,12 +5,12 @@
 }:
 rustPlatform.buildRustPackage rec {
   pname = "nu_plugin_desktop_notifications";
-  version = "1.2.4";
+  version = "1.2.4-unstable-2025-02-15";
 
   src = fetchFromGitHub {
     owner = "FMotalleb";
     repo = pname;
-    tag = version;
+    rev = "f999cc7e2d4444152c130f588c1293f68ecaca2e";
     sha256 = "sha256-jk5bta05+oouDWc/xiB49qeEqu4Al98WWJ8f0kuruYE=";
   };
   useFetchCargoVendor = true;
