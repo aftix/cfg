a6bafc36c7cc06ca944d84c6ef1f5eae3fe7245e
aftix <aftix@aftix.xyz>
2025-03-15 15:30:28 -0500

    flake.nix: Factored out several attributes to separate files
    This is part of an effort to make this entire repo work with or without
    flakes. The main parts left is to refactor the nixosConfigurations
    so there is only a configuration.nix, no entry.nix, and to
    write some spaghetti code to fetch the inputs. This will require
    finding out exactly what parts of each input the repo uses, and making
    an inputs attrset schema which only has that part. Then, a compatibility
    expression to fetch what's needed based on flake.lock in non-flake mode,
    and a function which extracts the needed schema from the flake schema.

---
+++ nixpkgs-cfg.nix
@@ -0,0 +1,25 @@
+{
+  inputs,
+  overlay ? (import ./overlay.nix inputs),
+}: {
+  nixpkgs = {
+    overlays = [
+      inputs.nur.overlays.default
+      inputs.attic.overlays.default
+      inputs.lix-module.overlays.default
+      overlay
+    ];
+    config = {
+      allowUnfreePredicate = pkg:
+        builtins.elem (inputs.nixpkgs.lib.getName pkg) [
+          "discord"
+          "pay-by-privacy"
+          "aspell-dict-en-science"
+        ];
+
+      permittedInsecurePackages = [
+        "jitsi-meet-1.0.8043"
+      ];
+    };
+  };
+}
