dbc37370bc6c46dc8435d6482b785888594ed00d
aftix <aftix@aftix.xyz>
2025-03-16 10:03:44 -0500

    Back out "overlay: Added fix for pwvucontrol from https://github.com/NixOS/nixpkgs/pull/389740"
    This backs out commit eea3ae4b7d8e1520abfd5c079b4c2f3fb2c69fe3.

--- overlay.nix
+++ overlay.nix
@@ -23,40 +23,6 @@ inputs: final: prev:
   });
 
   freshrssExts = final.lib.attrsets.recurseIntoAttrs (final.callPackage ./legacyPackages/freshrss {});
-
-  # From https://github.com/NixOS/nixpkgs/pull/389740
-  # See https://nixpk.gs/pr-tracker.html?pr=389740 for when it lands
-  pwvucontrol = let
-    wireplumber_0_4_patched = final.wireplumber.overrideAttrs (attrs: rec {
-      version = "0.4.17";
-      src = final.fetchFromGitLab {
-        domain = "gitlab.freedesktop.org";
-        owner = "pipewire";
-        repo = "wireplumber";
-        tag = version;
-        hash = "sha256-vhpQT67+849WV1SFthQdUeFnYe/okudTQJoL3y+wXwI=";
-      };
-
-      patches = [
-        (final.fetchpatch {
-          url = "https://gitlab.freedesktop.org/pipewire/wireplumber/-/commit/f4f495ee212c46611303dec9cd18996830d7f721.patch";
-          hash = "sha256-dxVlXFGyNvWKZBrZniFatPPnK+38pFGig7LGAsc6Ydc=";
-        })
-      ];
-    });
-  in
-    prev.pwvucontrol.overrideAttrs {
-      buildInputs = with final; [
-        cairo
-        gdk-pixbuf
-        glib
-        gtk4
-        libadwaita
-        pango
-        pipewire
-        wireplumber_0_4_patched
-      ];
-    };
 }
 // prev.lib.filesystem.packagesFromDirectoryRecursive {
   inherit (final) callPackage;
