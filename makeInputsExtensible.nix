4c5bf497a7668753b67be86a0b434766e11c4fa9
aftix <aftix@aftix.xyz>
2025-03-17 16:46:33 -0500

    default.nix,flake-compat/inputs.nix: Make inputs overridable by downstream users

---
+++ makeInputsExtensible.nix
@@ -0,0 +1,12 @@
+lib: let
+  makeInputsExtensible =
+    # Based off of lib.makeExtensibleWithCustomname
+    # from https://github.com/NixOS/nixpkgs/blob/2ba42c60e00e2fb01dac1917439c55e199661f8c/lib/fixed-points.nix#L444:C3
+    # MIT Licensed
+    # Allows the inputs to be overriden by downstream code
+    rattrs:
+      lib.fix' (
+        self: (rattrs self) // {overrideInputs = f: makeInputsExtensible (lib.extends (lib.toExtension f) rattrs);}
+      );
+in
+  makeInputsExtensible
