1d104d92ad9dbc5b5c9492c05a05721f99352cdd
aftix <aftix@aftix.xyz>
2025-03-19 19:02:26 -0500

    default.nix: Move makeFlake function to library

--- default.nix
+++ default.nix
@@ -1,13 +1,4 @@
 let
   inputs = import ./flake-compat/inputs.nix;
-  inherit (inputs.nixpkgs) lib;
-
-  makeFlake = inps:
-    lib.fix' (self:
-      inps.self
-      // {
-        inputs = builtins.removeAttrs inps ["self"];
-        overrideInputs = f: makeFlake (self.inputs.overrideInputs f);
-      });
 in
-  makeFlake inputs
+  inputs.self.lib.makeFlake inputs
