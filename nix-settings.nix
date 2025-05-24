dfa3cb2d9ba56b0d8b559cd2fc001820cad12687
aftix <aftix@aftix.xyz>
2025-05-23 21:12:33 -0500

    Removed attic substituter from nix-settings as it is often down and blocking builds

--- nix-settings.nix
+++ nix-settings.nix
@@ -5,12 +5,10 @@
   substituters = [
     "https://nix-community.cachix.org"
     "https://cache.nixos.org"
-    "https://attic.aftix.xyz/cfg-actions"
   ];
   trusted-public-keys = [
     "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-    "cfg-actions:R9aJEQdcJT8NrUh1yox2FgZfmzRrKi6MAobbfuRvv3g="
   ];
   extra-experimental-features = [
     "nix-command"
