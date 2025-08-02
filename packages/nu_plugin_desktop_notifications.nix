005ee5243cf911b0a7342ebc5afbb66313cd9835
aftix <aftix@aftix.xyz>
2025-08-02 12:37:42 -0500

    legacyPackages.nu_plugin_desktop_notifications: update to latest branch

--- packages/nu_plugin_desktop_notifications.nix
+++ packages/nu_plugin_desktop_notifications.nix
@@ -8,15 +8,15 @@
 }:
 rustPlatform.buildRustPackage rec {
   pname = "nu_plugin_desktop_notifications";
-  version = "1.2.4-unstable-2025-05-05";
+  version = "0.106.1-unstable-2025-08-01";
 
   src = fetchFromGitHub {
     owner = "FMotalleb";
     repo = pname;
-    rev = "de4464bf6ce6503977ee2bd41f11aeabc49214aa";
-    sha256 = "sha256-ZQ1zOYcGTfHhRAnDxVxcZ740yF2nccIOWL+yuShhs0Y=";
+    rev = "df62f36b34098c8060b5d86904368123603da094";
+    sha256 = "sha256-7ROby3TwpgCkEtMLeLNts6vG/XV6hBpUJHtiUJMXcoM=";
   };
-  cargoHash = "sha256-DrHWdwljPsPkzbM9mok5x7gn6Op1ytwg67+HtcZg8G8=";
+  cargoHash = "sha256-54GkHmvKLhYD49bgis2cYWAcelyzTUqf0pEqQJK5/8c=";
 
   meta = with lib; {
     description = "A nushell plugin to send desktop notifications.";
