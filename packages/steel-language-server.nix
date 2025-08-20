8aadfef45ee8c915af82db96afb54b8d96e37099
aftix <aftix@aftix.xyz>
2025-08-20 17:50:41 -0500

    chore(legacyPackages.steel-language-server): update to latest branch

--- packages/steel-language-server.nix
+++ packages/steel-language-server.nix
@@ -14,15 +14,15 @@
 }:
 rustPlatform.buildRustPackage {
   pname = "steel-language-server";
-  version = "0-unstable-2025-08-02";
+  version = "0-unstable-2025-08-18";
 
   src = fetchFromGitHub {
     owner = "mattwparas";
     repo = "steel";
-    rev = "96175bcd029a113cb757d4012ee86743cfeb7ed7";
-    hash = "sha256-6jRat6rBOLbfcMnbH6hrpJGm3A9buND5wiy7Eroy82c=";
+    rev = "af152945614a8778f9af2c4e111dd1ded02316f1";
+    hash = "sha256-ErQmpDx1H23O7dU9bphHO16KlfG0cWQAamA7rK0jdGQ=";
   };
-  cargoHash = "sha256-odavZmfacQ7BY2ITiL8dEPafr7LPAAo8k687rBqvQn4=";
+  cargoHash = "sha256-tga3KBviXjWbZePpSRueiBb2KnDME87R+BMyVmuSQVg=";
 
   nativeBuildInputs = [makeWrapper openssl pkg-config] ++ lib.optionals stdenv.hostPlatform.isDarwin [rustPlatform.bindgenHook];
   buildInputs = [steel];
