7deffc2cef1318ae0c9f84e7a18ce8f51549a73f
aftix <aftix@aftix.xyz>
2025-08-20 17:31:04 -0500

    chore(legacyPackages.freshrssExts.official): update to latest branch

--- legacyPackages/freshrss/official.nix
+++ legacyPackages/freshrss/official.nix
@@ -9,13 +9,13 @@
 }:
 stdenvNoCC.mkDerivation (self: {
   pname = "freshrss-extensions-official";
-  version = "0-unstable-2025-08-01";
+  version = "0-unstable-2025-08-19";
 
   src = fetchFromGitHub {
     owner = "FreshRSS";
     repo = "Extensions";
-    rev = "a5309b51fe3c2cf0ca86689365b855d37f85b35b";
-    hash = "sha256-wajyPqfUxteACUSoAzStwAc4dctoe4B0Ad1/yQhGxe4=";
+    rev = "179d3e7f37c23c3930f188472bfab11b5413a31b";
+    hash = "sha256-HWNagwmo69NLy7YLlizbJ7Yay7Xoph+DHeVIMgmqowc=";
   };
 
   installPhase = import ./with-subdirs.nix self.src;
