a3471f8ff1423a3042f0d7e00009a0fa22c40557
aftix <aftix@aftix.xyz>
2025-08-29 15:42:40 -0500

    chore(legacyPackages.steel-language-server): update package using passthru.updateScript

--- packages/steel-language-server.nix
+++ packages/steel-language-server.nix
@@ -15,15 +15,15 @@
 }:
 rustPlatform.buildRustPackage {
   pname = "steel-language-server";
-  version = "0-unstable-2025-08-18";
+  version = "0-unstable-2025-08-29";
 
   src = fetchFromGitHub {
     owner = "mattwparas";
     repo = "steel";
-    rev = "af152945614a8778f9af2c4e111dd1ded02316f1";
-    hash = "sha256-ErQmpDx1H23O7dU9bphHO16KlfG0cWQAamA7rK0jdGQ=";
+    rev = "9668db4eb00b25b6f5cfddc27a4d1aed63ecce75";
+    hash = "sha256-Tpzsna8jZbsmNeFK/VoqTQTti82bXF6ugqHnd4bGZm0=";
   };
-  cargoHash = "sha256-tga3KBviXjWbZePpSRueiBb2KnDME87R+BMyVmuSQVg=";
+  cargoHash = "sha256-a7wene1oI2lhMUo8iguosXyk1G12bhrEdK7IJ/WgRq4=";
 
   nativeBuildInputs = [makeWrapper openssl pkg-config] ++ lib.optionals stdenv.cc.isClang [rustPlatform.bindgenHook];
   buildInputs = [steel];
