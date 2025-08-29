ec32f6be687d94b5a359751c10e37bd3c9371ff7
aftix <aftix@aftix.xyz>
2025-08-29 15:24:13 -0500

    chore(legacyPackages.nu_plugin_audio_hook): update package using passthru.updateScript

--- packages/nu_plugin_audio_hook.nix
+++ packages/nu_plugin_audio_hook.nix
@@ -12,15 +12,15 @@
 }:
 rustPlatform.buildRustPackage rec {
   pname = "nu_plugin_audio_hook";
-  version = "0.106.1-unstable-2025-08-17";
+  version = "0.106.1-unstable-2025-08-27";
 
   src = fetchFromGitHub {
     owner = "FMotalleb";
     repo = pname;
-    rev = "578c0140ee229796650a66bb391ef25fe24c2145";
-    sha256 = "sha256-2JVMuOwqB7dNbR/ZQtd8SkeTuFChsAy6te/Z2gUlm48=";
+    rev = "24bcb023ba9d5ee6ec0194dd55e7dbe385f0f945";
+    sha256 = "sha256-1r0RrjZNMh5Or9jNNcKDW85REieSswMLnTR9h/uu0VI=";
   };
-  cargoHash = "sha256-bUARtJ6KoWaNid4tHNPI/eVAdqsLXkTXusMoqEyJqXo=";
+  cargoHash = "sha256-hKxnXIITW9cnpgUlz8goMXu47ye37y9flM5bl+iyHdg=";
 
   nativeBuildInputs = [pkg-config] ++ lib.optionals stdenv.cc.isClang [rustPlatform.bindgenHook];
   buildInputs = [alsa-lib];
