3c6067ae9385e8add47de855de3e610c86d05cd0
aftix <aftix@aftix.xyz>
2025-09-07 11:37:26 -0500

    chore(legacyPackages.nu_plugin_desktop_notifications): update package using passthru.updateScript

--- packages/nu_plugin_desktop_notifications.nix
+++ packages/nu_plugin_desktop_notifications.nix
@@ -9,15 +9,15 @@
 }:
 rustPlatform.buildRustPackage rec {
   pname = "nu_plugin_desktop_notifications";
-  version = "0.106.1-unstable-2025-08-29";
+  version = "0.107.0-unstable-2025-09-07";
 
   src = fetchFromGitHub {
     owner = "FMotalleb";
     repo = pname;
-    rev = "b0725dd00d434528825d1966d514c2b4b325afb7";
-    sha256 = "sha256-SkwbWzgIcsMCHVkTR5zGsLa5lCCy1VDnvZX3PUq5t20=";
+    rev = "cacfd2566a9a49744728b9984d88679681874c00";
+    sha256 = "sha256-KySh9eR090VebuHgO6Mi720wB2l9HwKj9eKCbG3vMQ4=";
   };
-  cargoHash = "sha256-KorNKRngFHWKdhs3r4MDiTotSZZ/3qBnLTpLtdopIZ4=";
+  cargoHash = "sha256-ZBZ6V9NTZGq6A/XSxi/pgrzsQYe46q6phx9WfT/b2vw=";
 
   passthru.updateScript = nix-update-script {
     extraArgs = ["--version" "branch"];
