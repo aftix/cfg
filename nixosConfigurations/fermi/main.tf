3f281794e68f23e02816954b8a19d55f4b82a7a1
aftix <aftix@aftix.xyz>
2026-06-20 23:54:18 -0500

    fix(fermi/main.tf): do not configure ACME DNS challenges

--- nixosConfigurations/fermi/main.tf
+++ nixosConfigurations/fermi/main.tf
@@ -69,23 +69,3 @@ resource "hcloud_zone_rrset" "fermiv6" {
   ttl = 600
   change_protection = false
 }
-
-resource "hcloud_zone_rrset" "txts" {
-  for_each = tomap({
-    "_acme-challenge.www.searx"   = "2ELjSxsV9Ifu5sTM00waExp4WzUHj_9jkz8SckGrAWQ"
-    "_acme-challenge.www.rss"     = "wYqI00an3euEN9gJQirdWgvZFgI4yipQGqc3R-igPDE"
-    "_acme-challenge.www.rss"     = "SUqDIQ2eEx4Q9p_0Q05x4FvzxrIWvAwT4AeW4GqSp6M"
-    "_acme-challenge.www.metrics" = "9CdEQQYxuhrB66DAAYuxTvIYBdkhoIgf87lCHf3gPAw"
-    "_acme-challenge.www.grocy"   = "caRTMlhnjiAo-vE8yNk1-aonYBmicxtYaUJ7U23IzRo"
-    "_acme-challenge.www.attic"   = "F_YKbpi2nDw4RJgUgFAVsUxNJENwSU4rIvKegTZSdtM"
-    "_acme-challenge.www.attic"   = "B8WABdWGqVs8sHGiLpMdxj2qpLfgaKKkib825rHPyq4"
-    "_acme-challenge.rss"         = "DMt2LqGinq5TJQwYgBU0SHPEeNenkOxJSiqnu5vvqdM"
-    "_acme-challenge"             = "y9SosxD88o7Nc1Ey-GfaUWgLLyEzlENJDjMHfhlxSr8"
-  })
-  zone = var.dns_zone_name
-  name = each.key
-  type = "TXT"
-  records = [{value = provider::hcloud::txt_record(each.value)}]
-  ttl = 300
-  change_protection = false
-}
